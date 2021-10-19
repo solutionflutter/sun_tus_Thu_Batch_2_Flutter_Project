f9b9737edaddc78f664690dff02b7dea2491b0d0
solutionflutter <solutionflutter@gmail.com>
2021-10-19 16:02:19 +0600

    ignire faile and change in Profilepage and postpage

--- lib/Messenger_Page.dart
+++ lib/Messenger_Page.dart
@@ -11,6 +11,32 @@ class Messenger_Page extends StatefulWidget {
 class _Messenger_PageState extends State<Messenger_Page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: ,
+        actions: [
+          Container(
+            child: FlatButton.icon(
+              onPressed: (){},
+              icon: Icon(
+                Icons.message,
+                size: 15,
+                color: Color.fromRGBO(31, 31, 45,0.6),
+              ),
+              label: Text(
+                  "Messages",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(31, 31, 31, 0.6)
+                ),
+              ),
+            ),
+          )
+        ],
+        centerTitle: ,
+        title: ,
+      ),
+    );
   }
 }
