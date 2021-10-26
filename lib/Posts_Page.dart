bdd9ceeb91bc298df28b0c81fda3fd24aefb1ced
solutionflutter <solutionflutter@gmail.com>
2021-10-26 16:28:15 +0600

    new code have  pushed

--- lib/Posts_Page.dart
+++ lib/Posts_Page.dart
@@ -4,16 +4,16 @@ import 'package:suntusthsbatchsecond/Models/Models.dart';
 import 'package:suntusthsbatchsecond/profile_page.dart';
 
 List<PostModel> post = [
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna Government Mahila College',address: ' Jalil - Sarani, Khulna,Khulna,Bangaldesh',postImageURL: '',likes: 2345),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna College',address: 'Seikh Para Main Rd, Khulna,Khulna,Bangladesh',postImageURL: '',likes: 345),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Ahsanullah College',address: 'KDA Ave, Khulna,Khulna,Bangladesh',postImageURL: '',likes: 545),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Azam Khan Government Commerce College',address: 'Babu Khan Rd, Khulna,Khulna,Bangladesh',postImageURL: '',likes: 45),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Govt. Brajalal (BL) College',address: 'Kashipur, B.L. College Rd, Khulna,Khulna,Bangladesh',postImageURL: '',likes: 35),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Govt. M. M. City College',address: '300 Khan Jahan Ali Rd, Khulna-9100,Khulna,Bangladesh',postImageURL: '',likes: 23),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna Public College',address: 'public college, road, Khulna-9000,Khulna,Bangaldesh',postImageURL: '',likes: 845),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: '',address: '',postImageURL: '',likes: 5355),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: '',address: '',postImageURL: '',likes: 4845),
-  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: '',address: '',postImageURL: '',likes: 245),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna Government Mahila College',address: ' Jalil - Sarani, Khulna,Khulna,Bangaldesh',postImageURL: 'images/Posts/post_1.jpg',likes: 2345),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna College',address: 'Seikh Para Main Rd, Khulna,Khulna,Bangladesh',postImageURL: 'images/Posts/post_2.jpg',likes: 345),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Ahsanullah College',address: 'KDA Ave, Khulna,Khulna,Bangladesh',postImageURL: 'images/Posts/post_3.jpg',likes: 545),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Azam Khan Government Commerce College',address: 'Babu Khan Rd, Khulna,Khulna,Bangladesh',postImageURL: 'images/Posts/post_4.jpg',likes: 45),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Govt. Brajalal (BL) College',address: 'Kashipur, B.L. College Rd, Khulna,Khulna,Bangladesh',postImageURL: 'images/Posts/post_5.jpg',likes: 35),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Govt. M. M. City College',address: '300 Khan Jahan Ali Rd, Khulna-9100,Khulna,Bangladesh',postImageURL: 'images/Posts/post_6.jpg',likes: 23),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna Public College',address: 'public college, road, Khulna-9000,Khulna,Bangladesh',postImageURL: 'images/Posts/post_7.jpg',likes: 845),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna Government College',address: 'Hasanbag Road, Khulna,Khulna,Bangladesh',postImageURL: 'images/Posts/post_8.jpg',likes: 5355),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Government Sundarban Adarsha College',address: '91 Khanjhan Ali road, Jessore-Dhaka Highway, Khulna,Bangladesh',postImageURL: 'images/Posts/post_9.jpg',likes: 4845),
+  PostModel(username: 'Sheikh Hasina',profileImageURL: 'images/Sheikh-Hasina.jpg',caption: 'Khulna Islamia Degree College',address: ' M A Bari St, Khulna,Khulna,Bangladesh',postImageURL: 'images/Posts/post_10.jpg',likes: 245),
 ];
 
 
@@ -78,19 +78,89 @@ class _Post_PageState extends State<Post_Page> {
                 horizontal: 15,
                 vertical: 15
             ),
-            itemCount: ,
+            itemCount: post.length,
             itemBuilder: (BuildContext context,int index){
                return Container(
-                 height: MediaQuery.of(context).size.height/3.5,
+                 height: MediaQuery.of(context).size.height/1.5,
                  width: MediaQuery.of(context).size.width,
                  margin: EdgeInsets.symmetric(vertical: 15),
                  decoration: BoxDecoration(
+                   gradient: LinearGradient(
+                     begin: Alignment.topRight,
+                     end: Alignment.bottomLeft,
+                     colors: [
+                       Color.fromRGBO(245, 12, 104, .1),
+                       Color.fromRGBO(245, 12, 104, .2),
+                       Color.fromRGBO(245, 12, 104, .3),
+                       Color.fromRGBO(245, 12, 104, .4),
+                       Color.fromRGBO(245, 12, 104, .5),
+                       Color.fromRGBO(245, 12, 104, .6),
+                       Color.fromRGBO(245, 12, 104, .7),
+                       Color.fromRGBO(245, 12, 104, .8),
+                       Color.fromRGBO(245, 12, 104, .9),
+                       Color.fromRGBO(245, 12, 104, 1),
+                     ]
+                   ),
                    borderRadius: BorderRadius.circular(15),
                  ),
-                 child: Column(
-                   children: [
+                 child: Padding(
+                   padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                   child: Column(
+                     children: [
 
-                   ],
+                       // Header Row
+
+                       Padding(
+                         padding: const EdgeInsets.symmetric(vertical: 7),
+                         child: Row(
+                           children: [
+                             CircleAvatar(
+                               radius: 30,
+                               backgroundImage: AssetImage("${post[index].profileImageURL}"),
+                             ),
+                             Padding(
+                               padding: EdgeInsets.symmetric(horizontal: 13),
+                               child: RichText(
+                                 textAlign: TextAlign.justify,
+                                 text: TextSpan(
+                                   text: "${post[index].username}\n",
+                                   style: TextStyle(
+                                     fontSize: 18,
+                                     fontWeight: FontWeight.w700,
+                                     color: Color.fromRGBO(21, 21, 21, 1),
+                                     height: 1.2,
+                                   ),
+                                   children: [
+                                     TextSpan(
+                                       text: "${DateTime.now()}",
+                                       style: TextStyle(
+                                         fontWeight: FontWeight.w500,
+                                         fontSize: 15,
+                                         color: Color.fromRGBO(21, 21, 21, 1)
+                                       ),
+                                     )
+                                   ],
+                                 ),
+                               ),
+                             ),
+                           ],
+                         ),
+                       ),
+
+                       //Address Row
+                       Padding(
+                         padding: EdgeInsets.symmetric(vertical: 7),
+                         child: Row(
+                           children: [
+                             Icon(
+                               Icons.location_on,
+                               size: ,
+                             ),
+                           ],
+                         ),
+                       )
+                     ],
+                   ),
                  ),
                );
             }
