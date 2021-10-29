1fd9b7eeb79e9f38d6453963e9fc4cbeb00619fb
solutionflutter <solutionflutter@gmail.com>
2021-10-29 14:25:20 +0600

    28_10_2021 Source code have pushed

--- lib/Following_Page.dart
+++ lib/Following_Page.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:suntusthsbatchsecond/Models/Models.dart';
 import 'package:suntusthsbatchsecond/profile_page.dart';
+
+List<Following> following = [
+  Following(username: "Abdur Rajjak", profileImageURL: "images/Profile_Images/user_1.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Shakib Al Hasan", profileImageURL: "images/Profile_Images/user_2.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Tawsif Mahbub", profileImageURL: "images/Profile_Images/user_3.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Mosharraf Karim", profileImageURL: "images/Profile_Images/user_4.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Chanchal Chowdhury", profileImageURL: "images/Profile_Images/user_5.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Afran Nisho", profileImageURL: "images/Profile_Images/user_6.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Ziaul Faruq Apurba", profileImageURL: "images/Profile_Images/user_7.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Siam Ahmed", profileImageURL: "images/Profile_Images/user_8.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Zakia Bari Mamo", profileImageURL: "images/Profile_Images/user_9.jpg", dateTime: DateTime.now().toUtc().toString()),
+  Following(username: "Mashrafe Mortaza", profileImageURL: "images/Profile_Images/user_10.jpg", dateTime: DateTime.now().toUtc().toString()),
+];
+
 
 class Following_Page extends StatefulWidget {
   const Following_Page({Key key}) : super(key: key);
@@ -55,6 +70,40 @@ class _Following_PageState extends State<Following_Page> {
             },
           ),
         ],
+      ),
+      body: Align(
+        alignment: Alignment.centerLeft,
+        child: ListView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            itemCount: following.length,
+            itemBuilder: (BuildContext context,int index){
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 35,
+                      backgroundImage: AssetImage("${following[index].profileImageURL}"),
+                    ),
+                    Expanded(child: RichText(
+                          textAlign: TextAlign.justify,
+                          text: TextSpan(
+                            text: "${following[index].username}",
+                            style: Text,
+                            children: [
+                              TextSpan(
+                                text: ,
+                                style: ,
+                              )
+                            ],
+                          ),
+                        )
+                    ),
+                  ],
+                )
+              );
+            },
+        )
       ),
     );
   }
---
+++ lib/Models/FollowingModel.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/foundation.dart';
+
+class Following{
+
+  Following({
+    @required this.username,
+    @required this.profileImageURL,
+    @required this.dateTime,
+  });
+
+  String username;
+  String profileImageURL;
+  String dateTime;
+}
